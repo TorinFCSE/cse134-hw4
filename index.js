91f50d30bb6053f86b42d742905b05988b039fb5
TorinFCSE <tfaes@ucsd.edu>
2023-11-29 22:50:52 -0800

    Dark mode for form bg

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ darkModeToggle.addEventListener('click', () => {
         document.body.style.setProperty('--text-color', 'white');
         document.body.style.setProperty('--body-bg-color', 'black');
         document.body.style.setProperty('--nav-bg-color', 'color(display-p3 0.15 0.5 0.7)');
+        document.body.style.setProperty('--form-bg-color', '#c0a080');
     } else {
         document.body.style.setProperty('--text-color', 'black');
         document.body.style.setProperty('--body-bg-color', 'white');
         document.body.style.setProperty('--nav-bg-color', 'color(display-p3 0.15 0.5 0.9)');
+        document.body.style.setProperty('--form-bg-color', '#f5f5d');
     }
     darkmode=!darkmode;
 });
